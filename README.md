51e555012741b8484b5640f395bca0c62f0dc2a1
Leonardo Aranguren S <leoaranguren10@gmail.com>
2024-02-12 14:04:39 -0300

    Fixing env.go for running test properly

--- README.md
+++ README.md
@@ -21,18 +21,29 @@ Look inside Service pkg, exist an interface referencing a couple of functions th
 
 # How to Run App
 
-### With Go
+#### With Go
 
 -   Create .env file in src/cmd/main directory
 -   go mod download
 -   go run main.go inside src/cmd/main directory
 
-## Or Docker
+#### Or Docker
 
 -   Install [ Docker Engine ](https://docs.docker.com/engine/install/) :fire:
 -   In `root` execute the following...
 -   Build image `docker-compose build`
 -   Run container `docker-compose up -d`
 -   Go to the app [ App ](http://127.0.0.1:3005/json-placeholders)
+
+# How to test it? - Using Testing pkg 🧪
+
+-   Set APP_ENV variable, in the root execute `export APP_ENV=.env.test`
+-   Execute `go test ./...`
+
+#### use test coverage (indicates the proportion of your code that is covered by tests)
+
+-   Execute `go test -coverprofile=coverage.out ./...`
+-   Execute `go tool cover -html=coverage.out -o coverage.html`
+-   Load the coverage.html in the browser
 
 ### Start reading code, interpreting functionalities and programming :smile:
--- src/pkg/common/env/env.go
+++ src/pkg/common/env/env.go
@@ -1,17 +1,62 @@
 package env
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
+
 func init() {
-    // Load environment variables from .env file
-    if err := godotenv.Load(); err != nil {
-        log.Println("Error loading .env file")
+    // Get the path to the project root directory
+    rootDir := findRootDir()
+	envFile := os.Getenv("APP_ENV")
+	if envFile == "" {
+		envFile = ".env" // default to regular environment file
+	}
+
+    // Construct the path to the .env file in the root directory
+    pathToEnvFile := filepath.Join(rootDir, envFile)
+
+    fmt.Println("pathToEnvFile ==> ", pathToEnvFile)
+
+    // Load environment variables from the .env file
+    if err := godotenv.Load(pathToEnvFile); err != nil {
+        log.Printf("Error loading %s file: %v", envFile, err)
     }
+}
+
+func findRootDir() string {
+    // Start from the current directory
+    currentDir, err := os.Getwd()
+    if err != nil {
+        log.Fatalf("Error getting current directory: %v", err)
+    }
+
+    // Navigate up the directory tree until we find the root
+    for {
+        if _, err := os.Stat(filepath.Join(currentDir, "go.mod")); err == nil {
+            // Found the root directory
+            return currentDir
+        }
+
+        // Go up one directory
+        parentDir := filepath.Dir(currentDir)
+
+        // If we're already at the root directory (no parent), break the loop
+        if parentDir == currentDir {
+            break
+        }
+
+        // Update current directory to the parent directory
+        currentDir = parentDir
+    }
+
+    // If we couldn't find the root directory, return an empty string
+    return ""
 }
 
 
